816d4a708e801e490e18671c73963f3ed0f3e9e4
ivandrinconb <ivand.rinconb@uqvirtual.edu.co>
2026-05-14 19:16:38 -0500

    Cambio de ventana
    Cambio a ventana principal

--- Concierto/src/main/java/co/edu/uniquindio/concierto/HelloApplication.java
+++ Concierto/src/main/java/co/edu/uniquindio/concierto/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MenuAdministrador.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("InicioSesion.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("App De eventos");
         stage.setScene(scene);
